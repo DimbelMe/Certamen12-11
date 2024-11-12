ffe1e09eef13dd2b145156207152b512569374da
Alumnos <Dimbel.me@gmail.com>
2024-11-12 14:29:29 -0300

    Interfaz

--- app/src/main/java/com/example/certamen12_11/MainActivity.java
+++ app/src/main/java/com/example/certamen12_11/MainActivity.java
@@ -2,12 +2,25 @@
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    EditText peso, estatura, imc;
+    Spinner spinner;
 
+    Adapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.Spinner);
+        peso = findViewById(R.id.txtPeso);
+        estatura = findViewById(R.id.txtAltura);
+        imc = findViewById(R.id.txtIMC);
+
     }
+
 }
